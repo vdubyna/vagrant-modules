fd306b4de31c5f32aec0ff48500ab58847773c3f
Volodymyr Dubyna <vdubyna@speroteck.com>
2014-09-21 17:06:28 +0300

    Add teamcity install

--- centos/core/java.sh
+++ centos/core/java.sh
@@ -10,6 +10,11 @@ echo "-> Prepare"
 
 echo "-> Install"
 
-
+    yum -y install wget
+    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u65-b17/jre-7u65-linux-x64.rpm
+    echo "cca8862e49cf2e6c6e28e1987c59a2f7  jre-7u65-linux-x64.rpm" >> MD5SUM
+    md5sum -c MD5SUM
+    rpm -Uvh jre-7u65-linux-x64.rpm
+    rm -f jre-7u65-linux-x64.rpm MD5SUM
 
 echo "-> Success"
--- centos/core/teamcity.sh
+++ centos/core/teamcity.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-echo "Install TeamCity"
-    wget http://download-cf.jetbrains.com/teamcity/TeamCity-8.1.4.tar.gz
-    tar -zxvf TeamCity-8.1.4.tar.gz
-    ./TeamCity/bin/runAll.sh start
+echo "-> Prepare"
+    if [[ -d /opt/TeamCity ]]; then
+        echo "TeamCity already installed"
+        exit 0
+    fi
 
+echo "Install TeamCity"
+
+    wget -qO- http://download-cf.jetbrains.com/teamcity/TeamCity-8.1.4.tar.gz | tar xz -C /opt
+    /opt/TeamCity/bin/teamcity-server.sh run
 
 echo "-> Success"
